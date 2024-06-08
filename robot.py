5d990db757e3b1b43435dcee05adbb746edb469e
Ricky Junaidi <106050611+RickyJ2@users.noreply.github.com>
2024-06-08 14:54:49 +0700

    add lypunov Control

--- robot.py
+++ robot.py
@@ -1,38 +1,73 @@
+from time import sleep
 import pygame
 import math
 from display import GREEN
 
+#constant
+p2m = 1
+MANUAL_MODE = 1
+LYAPUNOV_MODE = 2
+max = 0.05
+
 class Robot:
-    def __init__(self, startPos, width, height):
-        self.body = pygame.Rect(
-            startPos[0] - width/2, 
-            startPos[1] - height/2, 
-            width, 
-            height)
+    def __init__(self, startPos, width):
         self.x = startPos[0]
         self.y = startPos[1]
         self.width = width
-        self.height = height
-        self.theta = 0
+        self.body = pygame.image.load("robot.png")
+        self.theta = math.radians(0)
         self.vLeft = 0
         self.vRight = 0
+        self.rotated = self.body
+        self.rect = self.rotated.get_rect(center=(self.x, self.y))
+        self.mode = MANUAL_MODE
+        self.targetPos = startPos
 
     def draw(self, map):
-        map.drawRect(GREEN, pygame.Rect(
-                self.x - self.width/2, 
-                self.y - self.height/2, 
-                self.width, 
-                self.height)
-            )
+        map.blit(self.rotated, self.rect)
         
-    def move(self, event = None):
+    def move(self, dt, event = None):
         if event is not None:
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.targetPos = pygame.mouse.get_pos()
+                self.mode = LYAPUNOV_MODE
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.y += 1
-                if event.key == pygame.K_DOWN:
-                    self.y -= 1
-                if event.key == pygame.K_LEFT:
-                    self.x -= 1
-                if event.key == pygame.K_RIGHT:
-                    self.x += 1
+                if event.key == pygame.K_q:
+                    self.vLeft += 0.001
+                elif event.key == pygame.K_a:
+                    self.vLeft -= 0.001
+                elif event.key == pygame.K_e:
+                    self.vRight += 0.001
+                elif event.key == pygame.K_d:
+                    self.vRight -= 0.001
+                self.mode = MANUAL_MODE
+        #x,y start 0,0 from top left
+        if self.mode == LYAPUNOV_MODE:
+            vT, omegaT = self.LyapunovControl([self.targetPos[0], self.targetPos[1], math.radians(0)])
+            self.vLeft = vT - omegaT*self.width/2
+            self.vRight = vT + omegaT*self.width/2
+        self.vLeft = max if self.vLeft > max else -max if self.vLeft < -max else self.vLeft
+        self.vRight = max if self.vRight > max else -max if self.vRight < -max else self.vRight
+        v = ((self.vLeft + self.vRight)/2)
+        omega = ((self.vRight - self.vLeft)/self.width)
+        self.x += v * math.cos(self.theta)
+        self.y += v * math.sin(self.theta)
+        self.theta += omega
+        print("v Left: ", self.vLeft,"v Right: ", self.vRight, "v: ", v, "omega: ", omega, "dx: ", abs(self.x - self.targetPos[0]), "dy: ", abs(self.y - self.targetPos[1]), "dTheta: ", abs(self.theta - 0))
+        self.rotated = pygame.transform.rotozoom(self.body, 360 - math.degrees(self.theta),1)
+        self.rect = self.rotated.get_rect(center=(self.x, self.y))
+        # if self.mode == LYAPUNOV_MODE:
+        #     sleep(0.01)
+    def LyapunovControl(self, targetPoint):
+        errorX = targetPoint[0] - self.x
+        errorY = targetPoint[1] - self.y
+        errorTheta = targetPoint[2] - (self.theta)
+        if abs(errorX) < 5 and abs(errorY) < 5 and abs(errorTheta) < math.radians(5):
+            return 0, 0
+        k1 = 1
+        k2 = 8
+        k3 = 3
+        v = k1 * (errorX * math.cos(self.theta) + errorY * math.sin(self.theta))
+        omega = k2 * (-errorX * math.sin(self.theta) + errorY * math.cos(self.theta)) + k3 * (errorTheta)
+        
+        return v, omega
--- simulation.py
+++ simulation.py
@@ -7,8 +7,8 @@ import pygame
 #configuration
 XDIM = 30
 YDIM = 50
-POSROBOT_X = 10
-POSROBOT_Y = 2
+POSROBOT_X = 100
+POSROBOT_Y = 20
 GOALNODE_X = XDIM/2
 GOALNODE_Y = YDIM/2
 GOALRADIUS = 1
@@ -17,10 +17,13 @@ SCALE = 10
 class Simulation:
     def __init__(self):
         self.display = display([XDIM, YDIM],'Mobile Robot Simulator' ,SCALE)
-        object = readJsonFile("Object.json")
-        self.obs = convertJsonToObs(object)
-        self.robot = Robot([POSROBOT_X, POSROBOT_Y], 1.5, 1.5)
+        # object = readJsonFile("Object.json")
+        # self.obs = convertJsonToObs(object)
+        self.obs = []
+        self.robot = Robot([POSROBOT_X, POSROBOT_Y], 1.5)
         self.running = False
+        self.dt = 0
+        self.lastTime = pygame.time.get_ticks() 
     
     def run(self):
         posPlayer = [POSROBOT_X, POSROBOT_Y]
@@ -28,15 +31,18 @@ class Simulation:
         self.running = True
         while self.running:
             self.displayScreen(posPlayer)
+            self.dt = (pygame.time.get_ticks() - self.lastTime)/1000
+            self.lastTime = pygame.time.get_ticks()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                self.robot.move(event)
+                self.robot.move(self.dt, event)
+            self.robot.move(self.dt)
         pygame.quit()
 
     def displayScreen(self, posPlayer):
         self.clearDisplay()
-        self.robot.draw(self.display)
+        self.robot.draw(self.display.map)
         self.drawObs()
         self.updateDisplay()
     
